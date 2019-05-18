035813c7bcb4c87aee3ec048d511a84d92071ab5
Lancelot Prigent <lancelot@prigent.dev>
2019-05-18 10:54:09 +0200

    lint

---
+++ graphql/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends:  [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier',
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  parserOptions:  {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": false,
+    "@typescript-eslint/indent": false,
+  }
+};
