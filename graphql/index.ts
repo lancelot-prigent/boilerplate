7e69559011c6237a1a6f868b3ddfd38968de8d15
Lancelot Prigent <lancelot@prigent.dev>
2019-05-18 05:02:24 +0200

    add version to docker images

--- graphql/index.ts
+++ graphql/index.ts
@@ -3,7 +3,10 @@ import * as express from 'express';
 async function bootstrap() {
   const app = express();
   const port = process.env.PORT || 3000;
+  const commitRef = process.env.APP_COMMIT_REF || 'N/A';
+  const buildDate = process.env.APP_BUILD_DATE || new Date().toISOString();
 
+  app.get('/version', (req, res) => res.json({ commit: commitRef, buildDate }));
   app.get('/', (req, res) => res.send('Hello World!'));
 
   app.listen(port, () => {
